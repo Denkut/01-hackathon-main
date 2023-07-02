16e8f8f96bd629e79df5716e39cccbc860ee3215
Denis <kutepov.denisicloud.com@MacBook-Air-Denis.local>
2023-07-02 22:15:44 +0300

    добавил модули shape.module и timer.module

--- src/app.js
+++ src/app.js
@@ -2,10 +2,14 @@ import './styles.css';
 import { ContextMenu } from './menu';
 import { RandomSoundModule } from './modules/randomSound.module';
 import { BackgroundModule } from './modules/background.module';
+import { TimerModule } from './modules/timer.module';
+import { ShapeModule } from './modules/shape.module';
 
 const modules = [
     new RandomSoundModule("random-sound","Случайный звук"),
     new BackgroundModule('random-background', 'Случайный фон'),
+    new TimerModule('Таймер','Таймер'),
+    new ShapeModule('Рандомная фигура','Рандомная фигура')
 ];
 
 const contextMenu = new ContextMenu("#menu");
